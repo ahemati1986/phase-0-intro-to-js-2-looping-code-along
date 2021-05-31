c5020992fc8cc1d37e515dfe50823866951209ee
Abe Hemati <abehemati@gmail.com>
2021-05-31 13:45:08 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solutions in this file
+
+
+function writecards(["Ada", "Brendan", "Ali"], "birthday");
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    debugger;
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts); 
